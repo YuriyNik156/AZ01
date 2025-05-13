835fd554b38800046270ee3df89a3ffd42334b70
YuriyNik156 <yuriy.nik156@gmail.com>
2025-05-13 18:30:51 +0300

    add hometask2_mean_salary.py

--- hometask2_mean_salary.py
+++ hometask2_mean_salary.py
@@ -2,3 +2,10 @@
 
 import pandas as pd
 
+df = pd.read_csv("dz.csv") # Загрузка набора данных из CSV в DataFrame
+
+df.fillna(value = 0, inplace = True) # Замена пустых значений на 0.0
+
+group = df.groupby("City")["Salary"].mean() # Вывод средней зарплаты по городам
+
+print(group)
